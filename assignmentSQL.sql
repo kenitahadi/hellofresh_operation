3966a42efe06620ce197fcaf4372611fa8ad4c50
Kenita Hadi <77339736+kenitahadi@users.noreply.github.com>
2024-02-08 19:14:07 +0700

    Create assignmentSQL.sql

---
+++ assignmentSQL.sql
@@ -0,0 +1,22 @@
+--Request 1
+--For the customer with email address ‘ilovefood83@hotmail.com’ show all product_skus the
+--customer has an active subscription for
+
+
+--Request 2
+--Get a list of all the customers (id_customer) that have an active subscription to a product
+--that corresponds to a product family with product_family_handle = ‘classic-box’
+
+
+--Request 3
+--Count of customers that have ordered more than one product
+
+
+--Request 4
+--Get a list of all customers which got a box delivered to them two weeks ago, and the count
+--of boxes that had been delivered to them up to that week
+
+--Request 5
+--For all our customers, get the date of the latest order delivered to them and include
+--associated product_sku, delivery_date and purchase price. If there were two orders
+--delivered to the same customer on the same date, they should both appear
