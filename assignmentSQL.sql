a260cbc5cd83f1aee5dab316800c71be80c439c4
Kenita Hadi <77339736+kenitahadi@users.noreply.github.com>
2024-02-08 20:35:20 +0700

    Final assignmentSQL.sql

--- assignmentSQL.sql
+++ assignmentSQL.sql
@@ -21,16 +21,45 @@ JOIN product_family pf ON p.fk_product_family = pf.id_product_family
 WHERE s.status = 'active'
 AND pf.product_family_handle = 'classic-box';
 
-
 --Request 3
 --Count of customers that have ordered more than one product
 
+SELECT COUNT(DISTINCT o.id_customer) AS customers_with_multiple_orders
+FROM (
+    SELECT id_customer, COUNT(DISTINCT id_order) AS num_orders
+    FROM order
+    GROUP BY id_customer
+    HAVING num_orders > 1
+) AS o;
 
 --Request 4
 --Get a list of all customers which got a box delivered to them two weeks ago, and the count
 --of boxes that had been delivered to them up to that week
 
+SELECT o.id_customer, COUNT(DISTINCT o.id_order) AS count_of_boxes_delivered
+FROM `order` o
+WHERE o.delivery_date >= DATE_SUB(CURDATE(), INTERVAL 2 WEEK)
+    AND o.delivery_date < DATE_SUB(CURDATE(), INTERVAL 1 WEEK)
+GROUP BY o.id_customer;
+
 --Request 5
 --For all our customers, get the date of the latest order delivered to them and include
 --associated product_sku, delivery_date and purchase price. If there were two orders
 --delivered to the same customer on the same date, they should both appear
+
+SELECT o.id_customer,
+       p.product_SKU,
+       o.delivery_date,
+       o.purchase_price
+FROM `order` o
+JOIN product p ON o.fk_product = p.id_product
+JOIN (
+    SELECT id_customer,
+           delivery_date,
+           MAX(order_id) AS latest_order_id
+    FROM `order`
+    GROUP BY id_customer, delivery_date
+) AS latest_orders ON o.id_customer = latest_orders.id_customer 
+                   AND o.delivery_date = latest_orders.delivery_date 
+                   AND o.order_id = latest_orders.latest_order_id;
+
